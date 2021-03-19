73fda47614045cb6d6ed2e9224612e1b2f5dd009
Heloy <heloy.leal@hotmail.com>
2021-03-19 07:51:57 +0100

    relocated the components and the css files

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+import AddAppointments from './AddAppointments';
+import ListAppointments from './ListAppointments';
+import SearchAppointments from './SearchAppointments';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      myName : 'Heloy'
+    }
+  }
+  render() {
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                {this.state.myName}
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default App;
