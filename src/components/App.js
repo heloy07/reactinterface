d8bd15b13c627a4c5a9700331756c0fa62447b37
Heloy <heloy.leal@hotmail.com>
2021-03-19 09:49:16 +0100

    map pet names and owners from data.json

--- src/components/App.js
+++ src/components/App.js
@@ -5,20 +5,41 @@ import ListAppointments from './ListAppointments';
 import SearchAppointments from './SearchAppointments';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      myName : 'Heloy'
+      myAppointments: []
     }
   }
+  componentDidMount() {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          return item;
+        })
+        this.setState({
+          myAppointments: apts
+        });
+      });
+
+  }
   render() {
+    const listItems = this.state.myAppointments.map(item =>(
+      <div>
+
+      
+      <div>{item.petName}</div>
+      <div>{item.petName}</div>
+      </div>
+    ));
     return (
       <main className="page bg-white" id="petratings">
         <div className="container">
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
-                {this.state.myName}
+                {listItems}
                 <AddAppointments />
                 <SearchAppointments />
                 <ListAppointments />
